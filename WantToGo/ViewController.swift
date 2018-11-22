7fd887625b166ec8a67e87672309f480b9b763d6
zhangyunlong02 <zhangyunlong02@58ganji.com>
2018-11-22 10:49:13 +0800

    Initial Commit

---
+++ WantToGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WantToGo
+//
+//  Created by Muyuli on 2018/11/22.
+//  Copyright © 2018年 Muyuli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WantToGoTests/WantToGoTests.swift
@@ -0,0 +1,34 @@
+//
+//  WantToGoTests.swift
+//  WantToGoTests
+//
+//  Created by Muyuli on 2018/11/22.
+//  Copyright © 2018年 Muyuli. All rights reserved.
+//
+
+import XCTest
+@testable import WantToGo
+
+class WantToGoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
